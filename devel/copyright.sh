dcd3bd46b367f675d0d4f332ceada45f77f18210
Marcus Holland-Moritz <mhx@cpan.org>
2015-07-22 19:07:13 +0100

    Copyright update

--- devel/copyright.sh
+++ devel/copyright.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-FROM='-2011 Marcus'
-TO='-2013 Marcus'
+FROM='-2013 Marcus'
+TO='-2015 Marcus'
 for i in `grep -lEIr "Copyright.*$FROM.*Holland" . | grep -v ppport.h`; do
   echo $i
   perl -i.bak -pe "/Copyright.*Holland/ and s/$FROM/$TO/g" $i
