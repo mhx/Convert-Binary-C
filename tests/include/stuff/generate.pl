d52b17172680356bad33b82c7f76c08b0b438762
Marcus Holland-Moritz <mhx@cpan.org>
2020-11-19 17:13:02 +0100

    Use fewer, but bigger files (Windows is weird)

--- tests/include/stuff/generate.pl
+++ tests/include/stuff/generate.pl
@@ -6,10 +6,10 @@ use File::Spec;
 
 my $dir = shift // '.';
 
-for my $f ('aa' .. 'zz') {
+for my $f ('aa' .. 'az') {
   my $fh = IO::File->new(">" . File::Spec->catfile($dir, "$f.h")) or die;
   print $fh "/*\n";
-  for (1..200) {
+  for (1..2500) {
     print $fh $f x 36, "\n";
   }
   print $fh "*/\n";
